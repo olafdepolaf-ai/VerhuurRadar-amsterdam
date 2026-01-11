9534fa999d482334a648ebb520e2d5038ede6900
OlafL <olaflemmers@users.noreply.github.com>
2026-01-11 14:33:53 +0100

    security updates antigravity

--- services/firebase.ts
+++ services/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // Force Update: 1722424800000
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFfjJLmScGSqJ5lLPbNol63s3itbWxCQA",
-  authDomain: "amsterdam-vergunningen.firebaseapp.com",
-  projectId: "amsterdam-vergunningen",
-  storageBucket: "amsterdam-vergunningen.appspot.com",
-  messagingSenderId: "285598123174",
-  appId: "1:285598123174:web:e6f972adffdffd03a4a067"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
